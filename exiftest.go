3fccbff85c9ea05f12834886a18b4ae28d8a972f
ender <xuender@gmail.com>
2018-07-30 17:39:20 +0800

    exif

---
+++ exiftest.go
@@ -0,0 +1,17 @@
+package main
+import (
+	"fmt"
+	"github.com/xiam/exif"
+)
+
+func main() {
+	//data, err:= exif.Read("i.jpg")
+	data, err:= exif.Read("jddj.png")
+	if err!=nil{
+		fmt.Println(err.Error())
+	} else {
+		for key, val := range data.Tags {
+			fmt.Printf("%s = %s\n", key, val)
+		}
+	}
+}
