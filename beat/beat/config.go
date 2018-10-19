a30e13c53a96e1d46193c5ebd7d22049133c8f4a
ender <xuender@gmail.com>
2018-10-19 09:21:14 +0800

    reset

--- beat/beat/config.go
+++
@@ -1,5 +0,0 @@
-package beat
-
-// 配置
-type Config struct {
-}
--- room/src/proto.sh
+++
@@ -1,8 +0,0 @@
-protoc \
---plugin=protoc-gen-ts=/home/ender/.nvm/versions/node/v6.10.2/bin/protoc-gen-ts \
---js_out=import_style=commonjs,binary:generated \
---ts_out=service=true:generated \
--I ./proto \
-proto/*.proto
-
-protoc --go_out=../door -I ./proto proto/*.proto
