82cea03af824e9b5e76e084cf34882e6f3e820f7
ender <xuender@gmail.com>
2018-10-19 09:59:28 +0800

    feat: 配置读写,spf13 命令行

---
+++ cmd/config.go
@@ -0,0 +1,53 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// configCmd represents the config command
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: "配置文件读写测试",
+	Long:  `使用viper读写配置.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("config called")
+		if show, err := cmd.Flags().GetBool("show"); show && err == nil {
+			fmt.Println("显示配置")
+			fmt.Println(viper.GetString("name"))
+		}
+		viper.Set("time", "2018")
+		viper.WriteConfig()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(configCmd)
+	configCmd.Flags().BoolP("show", "s", false, "显示配置")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// configCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// configCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
